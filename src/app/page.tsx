a19d19f11102e6cadd0300438cd4c48a7f0c01c8
Juan Fernando <127075072+SweetArctic@users.noreply.github.com>
2025-03-15 20:37:22 -0500

    paleta de colores

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,24 @@
 const Home = () => {
   return (
     <div className="flex flex-wrap gap-4 p-4 justify-center items-center">
+      <div className="bg-white p-4 rounded-lg shadow-md w-96">
+        <h1 className="text-3xl font-bold text-center">Juan Fernando Rosero</h1>
+        <h1 className="text-3xl font-bold text-center">Configuracion de mi paleta de colores</h1>
+      </div>
       <span className="w-32 h-32 flex items-center justify-center text-white font-bold" style={{ backgroundColor: '#2F4156' }}>
-        Navy
+      #2F4156
       </span>
       <span className="w-32 h-32 flex items-center justify-center text-white font-bold" style={{ backgroundColor: '#557C8D' }}>
-        Teal
+      #557C8D
       </span>
       <span className="w-32 h-32 flex items-center justify-center text-gray-800 font-bold" style={{ backgroundColor: '#F5EEEB' }}>
-        Beige
+      #F5EEEB
       </span>
       <span className="w-32 h-32 flex items-center justify-center text-gray-800 font-bold" style={{ backgroundColor: '#C0D9E6' }}>
-        Sky Blue
+      #C0D9E6
       </span>
       <span className="w-32 h-32 flex items-center justify-center text-gray-800 font-bold border" style={{ backgroundColor: '#FFFFFF' }}>
-        White
+      #FFFFFF
       </span>
     </div>
   );
